b2b5f8ff8641463a1d5285d70d7fc54253acc3b4
Tim Spence <timothywspence@gmail.com>
2023-02-15 14:51:58 +0000

    Remove log line

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ use std::io::Write;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
